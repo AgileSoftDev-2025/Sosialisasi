c9014afd008f5b046bc638144cbf417ce368be2c
pilemon31 <barimbingpilemon@gmail.com>
2025-10-15 09:57:31 +0700

    feat: add register page

---
+++ sosialisasi-frontend/fe-sosialisasi/src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,19 @@
+import PageHead from "@/components/commons/PageHead";
+import { ReactNode } from "react";
+
+interface PropTypes {
+  title?: string;
+  children: ReactNode;
+}
+
+const AuthLayout = (props: PropTypes) => {
+  const { title, children } = props;
+  return (
+    <>
+      <PageHead title={title} />
+      <section className="min-h-screen w-full">{children}</section>
+    </>
+  );
+};
+
+export default AuthLayout;
