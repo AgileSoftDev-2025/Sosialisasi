5bce62054dd59c5936ce0d4d0854774a40d95d4c
pilemon31 <barimbingpilemon@gmail.com>
2025-10-17 10:52:56 +0700

    feat: fetch register schema

--- sosialisasi-frontend/fe-sosialisasi/src/pages/_app.tsx
+++ sosialisasi-frontend/fe-sosialisasi/src/pages/_app.tsx
@@ -1,11 +1,39 @@
 import "@/styles/globals.css";
+import { cn } from "./utils/cn";
 import { HeroUIProvider } from "@heroui/react";
 import type { AppProps } from "next/app";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const jakartaPlusSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  display: "swap",
+  variable: "--font-primary",
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <HeroUIProvider>
-      <Component {...pageProps} />
-    </HeroUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider>
+        <main
+          className={cn(
+            jakartaPlusSans.className,
+            "flex min-h-screen min-w-full flex-col items-center justify-center gap-10",
+          )}
+        >
+          <Component {...pageProps} />
+        </main>
+      </HeroUIProvider>
+    </QueryClientProvider>
   );
 }
