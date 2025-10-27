6e407bc00d8c747c94b1e68d31c20bb5df6cb8e0
pilemon31 <barimbingpilemon@gmail.com>
2025-10-27 19:34:41 +0700

    feat: add profile page

--- sosialisasi-frontend/fe-sosialisasi/src/components/layouts/DashboardLayout/DashboardLayout.constants.tsx
+++ sosialisasi-frontend/fe-sosialisasi/src/components/layouts/DashboardLayout/DashboardLayout.constants.tsx
@@ -2,25 +2,25 @@ export const SIDEBAR_ITEMS = [
   {
     key: "home",
     label: "Home",
-    href: "/home",
+    href: "/dashboard/home",
     icon: "fa-solid fa-home",
   },
   {
     key: "messages",
     label: "Messages",
-    href: "/messages",
+    href: "/dashboard/messages",
     icon: "fa-solid fa-message",
   },
   {
     key: "profile",
     label: "Profile",
-    href: "/profile",
+    href: "/dashboard/profile",
     icon: "fa-solid fa-user",
   },
   {
     key: "pengaturan",
     label: "Pengaturan",
-    href: "/settings",
+    href: "/dashboard/settings",
     icon: "fa-solid fa-gear",
   },
 ];
